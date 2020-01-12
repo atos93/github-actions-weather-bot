914e9bae748252be7b9fbb0a9ac7b5c03ab10919
Emanuele Deriu <deriuemanuele@hotmail.it>
2020-01-12 15:46:15 +0100

    fix

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ const bot = new Telegram(process.env.TELEGRAM_TOKEN)
 
 const weatherToken = process.env.WEATHER_API_TOKEN
 
-const weatherURL = new URL("https://api.openweathermap.org/data/2.5/weather")
-weatherURL.searchParams.set("zip", "09047,it")
-weatherURL.searchParams.set("APPID", weatherToken)
+const weatherURL = new URL('https://api.openweathermap.org/data/2.5/weather')
+weatherURL.searchParams.set('zip', '78747,us')
+weatherURL.searchParams.set('APPID', weatherToken)
+weatherURL.searchParams.set('units', 'imperial')
 
 const getWeatherData = async () => {
     const resp = await fetch(weatherURL.toString())
     const body = await resp.json()
+    return body
 }
 
 const generateWeatherMessage = weatherData =>
-    `The weather in ${wheaterData.name}: ${weatherData.weather[0].description}.
-    Current temperature is ${weatherData.main.temp}, with a low temp of ${weatherData.main.temp_min} 
-    and high of ${weatherData.main.temp_max}`
-
+    `The weather in ${weatherData.name}: ${weatherData.weather[0].description}. Current temperature is ${weatherData.main.temp}, with a low temp of ${weatherData.main.temp_min} and high of ${weatherData.main.temp_max}.`
 
 const main = async () => {
     const weatherData = await getWeatherData()
     const weatherString = generateWeatherMessage(weatherData)
     bot.sendMessage(process.env.TELEGRAM_CHAT_ID, weatherString)
-    console.log(weatherString)
 }
+
 main()
